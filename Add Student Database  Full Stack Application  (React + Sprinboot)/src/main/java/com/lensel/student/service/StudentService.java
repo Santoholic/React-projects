0c1e1f37bb647048ac083db2b1f5a5eca882e0cf
Santosh S <81346813+Santoholic@users.noreply.github.com>
2022-12-03 15:08:18 +0530

    Delete Add Student Database  Full Stack Application  (React + Sprinboot) directory

--- Add Student Database  Full Stack Application  (React + Sprinboot)/src/main/java/com/lensel/student/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.lensel.student.service;
-import com.lensel.student.model.Student;
-
-import java.util.List;
-
-public interface StudentService {
-    public Student saveStudent(Student student);
-    public List<Student> getAllStudents();
-}
